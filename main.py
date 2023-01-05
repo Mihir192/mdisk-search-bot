9e5a1eee47a4cb31b30a29d58a3b89fbce8daf49
Mihir192 <105259127+Mihir192@users.noreply.github.com>
2023-01-05 11:27:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ async def help_handler(_, event: Message):
 async def inline_handlers(_, event: Message):
     if event.text == '/start':
         return
-    answers = f'**📂 Results For ➠ {event.text} \n\n Join Our New Channel \n https://t.me/marathimoviezfree4 \n https://t.me/marathimoviezfree4 \n https://t.me/marathimoviezfree4 \n\n**'
+    answers =""
     async for message in User.search_messages(chat_id=Config.CHANNEL_ID, limit=50, query=event.text):
         if message.text:
             thumb = None
@@ -59,7 +59,7 @@ async def inline_handlers(_, event: Message):
             if "|||" in message.text:
                 f_text = message.text.split("|||", 1)[0]
                 msg_text = message.text.html.split("|||", 1)[0]
-            answers += f'**Not Found \n Join Our New Channel \n https://t.me/marathimoviezfree4 \n https://t.me/marathimoviezfree4 \n https://t.me/marathimoviezfree4 \n**'
+            answers += ""
     try:
         await event.reply_text(
             answers
