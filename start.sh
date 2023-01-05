3317d542449af3aa283a2b4c951e9b1a6e96a927
Mihir192 <105259127+Mihir192@users.noreply.github.com>
2023-01-05 12:07:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/PredatorHackerzZ/MessageSearchBot.git /MessageSearchBot
-cd /MessageSearchBot
+git clone https://github.com/Mihir192/mdisk-search-bot.git /mdisk-search-bot
+cd /mdisk-search-bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
